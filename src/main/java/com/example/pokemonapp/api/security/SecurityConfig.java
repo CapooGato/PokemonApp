ae92de4dc89b0719bc0d729ac5d5f34845bf3d9a
Semwerynek <kaw.seweryn@gmail.com>
2025-04-16 11:53:40 +0200

    Authorization based on roels in memory

--- src/main/java/com/example/pokemonapp/api/security/SecurityConfig.java
+++ src/main/java/com/example/pokemonapp/api/security/SecurityConfig.java
@@ -2,8 +2,14 @@ package com.example.pokemonapp.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,10 +23,29 @@ public class SecurityConfig {
         http
                 .csrf(csfr -> csfr.disable()) // Cross-site Request Forgery
                 .authorizeHttpRequests(authorizeHttpRequest ->
-                        authorizeHttpRequest.anyRequest().authenticated() // Authenticated = logged and in Security Context
+                        authorizeHttpRequest
+                                .requestMatchers(HttpMethod.GET).hasAnyRole("ADMIN", "USER")
+                                .anyRequest().authenticated() // Authenticated = logged and in Security Context
                 )
                 .httpBasic(withDefaults()); // Just HTTP authentication, not HTTPS
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
